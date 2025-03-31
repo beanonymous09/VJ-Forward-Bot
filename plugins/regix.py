4b08dac5b35e33e69b75ca07414a289bd73ef7a8
beanonymous09 <beanonymoushacker09@gmail.com>
2025-03-31 09:42:58 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -117,7 +117,7 @@ async def pub_(bot, message):
    
        if forwarded >= i.total_files:
            await client.delete_messages(i.TO, sts.get("status_msg_id"))
-          sts.remove("status_msg_id")
+           sts.remove("status_msg_id")
     except:
        await msg_edit(m, f"**Please Make Your [UserBot / Bot](t.me/{_bot['username']}) Admin In Target Channel With Full Permissions**", retry_btn(frwd_id), True)
        return await stop(client, user)
